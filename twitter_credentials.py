a7491db9c5bcde936361c4fefbe3d411a7e9709a
David Benshimol <48688095+dbenshimol@users.noreply.github.com>
2020-09-12 22:57:06 +0300

    Update twitter_credentials.py
    Remove credentials

--- twitter_credentials.py
+++ twitter_credentials.py
@@ -1,6 +1,6 @@
 # Twitter API authentication
 
-consumer_key = 'WCpUrDTz1DbtOP748UE0tsKFe'
-consumer_secret = 'QQEStPoqnan3PPKd5hjtDfkhMw6acn5IIX8xK4cRScMoZ7EPfC'
-access_token = '1550256049-m9HV9pk3rmffstH9U9MV6eI316UOt6t72ciH4jE'
-access_token_secret = 'JiOjDPbFvxzxVeMrSQmEU2xh3dQlnTkqgOJTFWRRHcvkj'
+consumer_key = ''
+consumer_secret = ''
+access_token = ''
+access_token_secret = ''
